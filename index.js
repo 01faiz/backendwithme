3ca91973507f1aa7a0f9f0d01c2b1f19685dd83b
Alfaiz Chadiyata <imalfaiz007@gmail.com>
2024-08-04 15:45:32 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT
+
+app.get('/', (req,res) => {
+    res.send('Hello Alfaiz')
+})
+
+app.get('/follow', (req,res) => {
+     res.send('Alfaiz Chadiyata')
+})
+
+app.listen(port , () =>{
+    console.log(`SERVER STARTED ON PORT NO ${port}`)
+})
