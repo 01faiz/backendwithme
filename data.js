754a7fd2df2b7b17ab267bb17ecc0ee3b887cdde
Alfaiz <102668382+01faiz@users.noreply.github.com>
2024-08-05 12:12:33 +0530

    Create data.js
    added data

---
+++ data.js
@@ -0,0 +1,274 @@
+const carts = [
+    {
+      id: 1,
+      products: [Array],        
+      total: 103774.85,
+      discountedTotal: 89686.65,
+      userId: 33,
+      totalProducts: 4,
+      totalQuantity: 15
+    },
+    {
+      id: 2,
+      products: [Array],        
+      total: 4794.8,
+      discountedTotal: 4288.95,
+      userId: 142,
+      totalProducts: 5,
+      totalQuantity: 20
+    },
+    {
+      id: 3,
+      products: [Array],
+      total: 16775.87,
+      discountedTotal: 14144.3,
+      userId: 108,
+      totalProducts: 5,
+      totalQuantity: 13
+    },
+    {
+      id: 4,
+      products: [Array],
+      total: 456.83,
+      discountedTotal: 443.23,
+      userId: 87,
+      totalProducts: 4,
+      totalQuantity: 17
+    },
+    {
+      id: 5,
+      products: [Array],
+      total: 7431.3,
+      discountedTotal: 6676.44,
+      userId: 134,
+      totalProducts: 6,
+      totalQuantity: 20
+    },
+    {
+      id: 6,
+      products: [Array],
+      total: 35199.86,
+      discountedTotal: 31435.05,
+      userId: 150,
+      totalProducts: 4,
+      totalQuantity: 14
+    },
+    {
+      id: 7,
+      products: [Array],
+      total: 145651.89,
+      discountedTotal: 128504.41,
+      userId: 86,
+      totalProducts: 3,
+      totalQuantity: 11
+    },
+    {
+      id: 8,
+      products: [Array],
+      total: 15685.86,
+      discountedTotal: 12666.75,
+      userId: 23,
+      totalProducts: 4,
+      totalQuantity: 14
+    },
+    {
+      id: 9,
+      products: [Array],
+      total: 65529.87,
+      discountedTotal: 63336.57,
+      userId: 194,
+      totalProducts: 4,
+      totalQuantity: 13
+    },
+    {
+      id: 10,
+      products: [Array],
+      total: 29499.92,
+      discountedTotal: 25675.13,
+      userId: 160,
+      totalProducts: 2,
+      totalQuantity: 8
+    },
+    {
+      id: 11,
+      products: [Array],
+      total: 568.36,
+      discountedTotal: 476.76,
+      userId: 172,
+      totalProducts: 6,
+      totalQuantity: 14
+    },
+    {
+      id: 12,
+      products: [Array],
+      total: 931.91,
+      discountedTotal: 798.02,
+      userId: 202,
+      totalProducts: 2,
+      totalQuantity: 9
+    },
+    {
+      id: 13,
+      products: [Array],
+      total: 793.07,
+      discountedTotal: 750.8,
+      userId: 41,
+      totalProducts: 5,
+      totalQuantity: 13
+    },
+    {
+      id: 14,
+      products: [Array],
+      total: 586.82,
+      discountedTotal: 500.25,
+      userId: 94,
+      totalProducts: 6,
+      totalQuantity: 18
+    },
+    {
+      id: 15,
+      products: [Array],
+      total: 11741.31,
+      discountedTotal: 10940.95,
+      userId: 11,
+      totalProducts: 5,
+      totalQuantity: 19
+    },
+    {
+      id: 16,
+      products: [Array],
+      total: 172.24,
+      discountedTotal: 155.84,
+      userId: 200,
+      totalProducts: 3,
+      totalQuantity: 6
+    },
+    {
+      id: 17,
+      products: [Array],
+      total: 192.87,
+      discountedTotal: 173.9,
+      userId: 141,
+      totalProducts: 4,
+      totalQuantity: 13
+    },
+    {
+      id: 18,
+      products: [Array],
+      total: 1464.78,
+      discountedTotal: 1257.94,
+      userId: 189,
+      totalProducts: 6,
+      totalQuantity: 22
+    },
+    {
+      id: 19,
+      products: [Array],
+      total: 933.87,
+      discountedTotal: 794.78,
+      userId: 59,
+      totalProducts: 4,
+      totalQuantity: 13
+    },
+    {
+      id: 20,
+      products: [Array],
+      total: 3534.78,
+      discountedTotal: 2936.58,
+      userId: 90,
+      totalProducts: 6,
+      totalQuantity: 22
+    },
+    {
+      id: 21,
+      products: [Array],
+      total: 251.96,
+      discountedTotal: 245.68,
+      userId: 42,
+      totalProducts: 2,
+      totalQuantity: 4
+    },
+    {
+      id: 22,
+      products: [Array],
+      total: 222.88,
+      discountedTotal: 205.63,
+      userId: 140,
+      totalProducts: 4,
+      totalQuantity: 12
+    },
+    {
+      id: 23,
+      products: [Array],
+      total: 7757.8,
+      discountedTotal: 7439.98,
+      userId: 147,
+      totalProducts: 5,
+      totalQuantity: 20
+    },
+    {
+      id: 24,
+      products: [Array],
+      total: 1749.9,
+      discountedTotal: 1594.33,
+      userId: 6,
+      totalProducts: 3,
+      totalQuantity: 10
+    },
+    {
+      id: 25,
+      products: [Array],
+      total: 412.98,
+      discountedTotal: 353.55,
+      userId: 118,
+      totalProducts: 2,
+      totalQuantity: 2
+    },
+    {
+      id: 26,
+      products: [Array],
+      total: 459.92,
+      discountedTotal: 441.17,
+      userId: 66,
+      totalProducts: 2,
+      totalQuantity: 8
+    },
+    {
+      id: 27,
+      products: [Array],
+      total: 190.89,
+      discountedTotal: 170.95,
+      userId: 75,
+      totalProducts: 4,
+      totalQuantity: 11
+    },
+    {
+      id: 28,
+      products: [Array],
+      total: 209.91,
+      discountedTotal: 186.09,
+      userId: 147,
+      totalProducts: 2,
+      totalQuantity: 9
+    },
+    {
+      id: 29,
+      products: [Array],
+      total: 3862.43,
+      discountedTotal: 3488.44,
+      userId: 170,
+      totalProducts: 5,
+      totalQuantity: 17
+    },
+    {
+      id: 30,
+      products: [Array],
+      total: 128249.07,
+      discountedTotal: 118740.76,
+      userId: 177,
+      totalProducts: 4,
+      totalQuantity: 13
+    }
+  ]
+
+  export {carts}
