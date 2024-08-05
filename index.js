2c9a3e035f71c3601bb63a5bb3065f423d853a70
Alfaiz <102668382+01faiz@users.noreply.github.com>
2024-08-05 20:10:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 const carts = require('./data.js');
+const cors = require("cors");
 
 
 const port = process.env.PORT
+
+app.use(cors());
 
 app.get('/', (req,res) => {
     res.send(carts.carts)
