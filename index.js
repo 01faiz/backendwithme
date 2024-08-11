9d73fcf948472744444cdbdbc4d64572ca31139f
Alfaiz Chadiyata <imalfaiz007@gmail.com>
2024-08-11 13:46:15 +0530

    added the data

--- index.js
+++ index.js
@@ -2,29 +2,19 @@ const express = require('express');
 const {carts} = require('./data.js');
 const cors = require("cors");
 const app = express();
-<<<<<<< HEAD
+
 const port = 3000 || process.env.PORT
    
 app.use(cors());
-=======
-const carts = require('./data.js');
-const cors = require("cors");
-
-
-const port = process.env.PORT
->>>>>>> 24f8cd5a9f30be95e8ecc065b404f7eeedbcb400
-
-app.use(cors());
 
 app.get('/', (req,res) => {
-<<<<<<< HEAD
+
     res.send("<h1>WELCOME TO HOME PAGE </h1>")
 })
+
 app.get('/user', (req,res) => {
     res.send(carts)
-=======
-    res.send(carts.carts)
->>>>>>> 24f8cd5a9f30be95e8ecc065b404f7eeedbcb400
+
 })
 
 app.get('/user/:id', (req,res) => {
